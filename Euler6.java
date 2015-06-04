d01a0339b2477dcb87dfa45d6a553cd7d3ffe191
s-frey <sarth.frey@gmail.com>
2015-06-03 23:54:55 -0400

    updated #6

--- Euler6.java
+++ Euler6.java
@@ -2,8 +2,21 @@
 public class Euler6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sumOfSquares = 0;
+		int instSquare= 0;
+		int squareOfSum = 0;
+		int sum = 0;
+		int answer = 0;
+		for (int i = 1; i <= 100; i++) {
+			instSquare = i * i;
+			sumOfSquares += instSquare;
+		}
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		squareOfSum = sum * sum;
+		answer = squareOfSum - sumOfSquares;
+		System.out.println(answer);
 	}
 
 }
